71456dfd27f583e4290b384c4779161f24cff6c8
Xin Tan <mactanxin@gmail.com>
2022-08-22 18:54:45 +0800

    feat(init): require impatient, bye coc

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,5 @@
 -- basics
+require("core.impatient")
 vim.cmd("set nocompatible")
 vim.cmd("syntax on")
 vim.cmd("filetype plugin indent on")
@@ -21,28 +22,10 @@ vim.cmd [[
 ]]
 vim.cmd [[
   set fillchars+=diff:\ 
-  hi CocSearch ctermfg=12 guifg=#18A3FF
-  hi CocMenuSel ctermbg=109 guibg=#13354A
-  " Highlight the symbol and its references when holding the cursor.
-  autocmd CursorHold * silent call CocActionAsync('highlight')
 
 if exists("g:loaded_webdevicons")
   call webdevicons#refresh()
 endif
-
-" use Tab
-function! s:check_back_space() abort
-  let col = col('.') - 1
-  return !col || getline('.')[col - 1]  =~# '\s'
-endfunction
-
-inoremap <silent><expr> <TAB>
-      \ pumvisible() ? "\<C-n>" :
-      \ <SID>check_back_space() ? "\<TAB>" :
-      \ coc#refresh()
-inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
-
-
 ]]
 
 vim.cmd [[
@@ -83,10 +66,9 @@ vim.g.cursorhold_updatetime = 100
 -- use emmet
 vim.g.user_emmet_mode = "a"
 -- vim.g.rooter_patterns = ['.git', 'package.json', 'yarn.lock'] 
-
 require("core.options")
 require("core.keymaps")
-require("core.coc")
+require("core.nvim-cmp")
 require("core.theme")
 require("core.plugins")
 require("core.treesitter")
@@ -120,36 +102,3 @@ local fn = vim.fn
 require("telescope").load_extension("session-lens")
 require('telescope').load_extension("notify")
 require('telescope').load_extension("file_browser")
-
-function coc_status_notify(msg, level)
-  local notify_opts = { title = "LSP Status", timeout = 500, hide_from_history = true, on_close = reset_coc_status_record }
-  -- if coc_status_record is not {} then add it to notify_opts to key called "replace"
-  if coc_status_record ~= {} then
-    notify_opts["replace"] = coc_status_record.id
-  end
-  coc_status_record = vim.notify(msg, level, notify_opts)
-end
-
-function reset_coc_status_record(window)
-  coc_status_record = {}
-end
-
-local coc_diag_record = {}
-
-function coc_diag_notify(msg, level)
-  local notify_opts = { title = "LSP Diagnostics", timeout = 500, on_close = reset_coc_diag_record }
-  -- if coc_diag_record is not {} then add it to notify_opts to key called "replace"
-  if coc_diag_record ~= {} then
-    notify_opts["replace"] = coc_diag_record.id
-  end
-  coc_diag_record = vim.notify(msg, level, notify_opts)
-end
-
-function reset_coc_diag_record(window)
-  coc_diag_record = {}
-end
-
-function coc_notify(msg, level)
-  local notify_opts = { title = "LSP Message", timeout = 500 }
-  vim.notify(msg, level, notify_opts)
-end
