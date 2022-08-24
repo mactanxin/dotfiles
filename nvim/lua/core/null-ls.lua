e0d962538074108bb2ac5928228b533a71cd627c
Xin Tan <mactanxin@gmail.com>
2022-08-24 19:04:49 +0800

    null-ls format on save using eslint_d

--- nvim/lua/core/null-ls.lua
+++ nvim/lua/core/null-ls.lua
@@ -1,22 +1,39 @@
-require("null-ls").setup({
-    debug = true,
-    sources = {
-        require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.diagnostics.eslint,
-        require("null-ls").builtins.completion.spell,
-    },
-  -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+	vim.notify("null-ls import failed", "error", { title = "plugin loader status" })
+	return
+end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local format = function(payload)
+	vim.lsp.buf.formatting({
+		filter = function(clients)
+			return vim.tbl_filter(function(client)
+				return client.name ~= "volar"
+			end, clients)
+		end,
+	})
+end
+
+null_ls.setup({
+	debug = true,
+	sources = {
+		formatting.eslint_d,
+		null_ls.builtins.diagnostics.eslint_d,
+		null_ls.builtins.completion.spell,
+	},
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = format,
+			})
+		end
+	end,
 })
