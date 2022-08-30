36625136319a4de8c4df8f1a738c5e08695ed239
Xin Tan <mactanxin@gmail.com>
2022-08-30 09:56:13 +0800

    delete vim-snippets

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -131,9 +131,6 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
-  -- required for coc-snip
-  use "honza/vim-snippets"
-
   -- LSPh
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer" -- simple to use language server installer
