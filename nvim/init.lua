3fddfb637d1091de10b6e33e1e66d2d2335d9109
Xin Tan <mactanxin@gmail.com>
2022-10-29 19:07:33 +0800

    globals.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,47 +1,4 @@
-vim.cmd([[
-  if exists("g:loaded_webdevicons")
-    call webdevicons#refresh()
-  endif
-]])
-
-vim.cmd([[
-
-"*****************************************************************************
-"" Abbreviations
-"*****************************************************************************
-"" no one is really happy until you have this shortcuts
-
-cnoreabbrev W! w!
-cnoreabbrev Q! q!
-cnoreabbrev Qall! qall!
-cnoreabbrev Wq wq
-cnoreabbrev Wa wa
-cnoreabbrev wQ wq
-cnoreabbrev WQ wq
-cnoreabbrev W w
-cnoreabbrev Q q
-cnoreabbrev Qall qall
-]])
-
-vim.g.auto_session_enabled = "v:false"
-vim.g.loaded_matchparen = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_shada_plugin = 1
-vim.g.loaded_spellfile_plugin = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_remote_plugins = 1
-vim.g.cursorhold_updatetime = 100
--- use emmet
-vim.g.user_emmet_mode = "a"
-
+require("core.globals")
 require("core.ayu")
 require("core.options")
 require("core.keymaps")
@@ -55,7 +12,6 @@ require("core.comment")
 require("core.cinnamon")
 require("core.autopairs")
 require("core.showMessage")
---[[ require("core.notify") ]]
 require("core.startup-screen")
 require("core.dashboard")
 require("core.neo-tree")
