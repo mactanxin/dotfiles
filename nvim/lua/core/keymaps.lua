cfd704b33a94180c3f863c6e52ace39f1daa970f
Xin Tan <mactanxin@gmail.com>
2022-10-29 19:07:45 +0800

    abbrs

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -33,6 +33,25 @@ local function toast(msg, level)
 		timeout = 5000,
 	})
 end
+
+vim.cmd([[
+
+"*****************************************************************************
+"" Abbreviations
+"*****************************************************************************
+"" no one is really happy until you have this shortcuts
+
+cnoreabbrev W! w!
+cnoreabbrev Q! q!
+cnoreabbrev Qall! qall!
+cnoreabbrev Wq wq
+cnoreabbrev Wa wa
+cnoreabbrev wQ wq
+cnoreabbrev WQ wq
+cnoreabbrev W w
+cnoreabbrev Q q
+cnoreabbrev Qall qall
+]])
 --Remap space as leader key
 --
 mapcmd("s", "<Nop>")
