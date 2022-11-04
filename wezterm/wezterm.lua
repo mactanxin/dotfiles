bace3325d509e69aca07244195284d6c56553c64
Xin Tan <mactanxin@gmail.com>
2022-11-04 12:07:29 +0800

    wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,24 @@
+local wezterm = require("wezterm")
+
+return {
+	color_scheme = "ayu",
+	font_size = 24.0,
+	font = wezterm.font("Fira Code"),
+	-- You can specify some parameters to influence the font selection;
+	-- for example, this selects a Bold, Italic font variant.
+	font = wezterm.font("JetBrains Mono", { weight = "Bold", italic = true }),
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		-- This will create a new split and run your default program inside it
+		{
+			key = "|",
+			mods = "CTRL",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			key = "-",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+	},
+}
