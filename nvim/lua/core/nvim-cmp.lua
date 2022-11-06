ca6fba581f8765f39028cdc2084987596f6b29e9
Xin Tan <mactanxin@gmail.com>
2022-11-06 19:44:57 +0800

    cmp-config with local luasnip imports, local has_words_before function, tab/s-tab keybinding, formatting with lspkind-nvim

--- nvim/lua/core/nvim-cmp.lua
+++ nvim/lua/core/nvim-cmp.lua
@@ -6,8 +6,27 @@ if not status_ok then
 end
 
 local cmp = require("cmp")
+local lspkind = require("lspkind")
+local luasnip = require("luasnip")
+
+local has_words_before = function()
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
 
 cmp.setup({
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol", -- show only symbol annotations
+			maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+			ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+			-- The function below will be called before any actual modifications from lspkind
+			-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+			before = function(entry, vim_item)
+				return vim_item
+			end,
+		}),
+	},
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
@@ -22,28 +41,28 @@ cmp.setup({
 		documentation = cmp.config.window.bordered(),
 	},
 	mapping = cmp.mapping.preset.insert({
-		--[[ ["<Tab>"] = cmp.mapping(function(fallback) ]]
-		--[[ 	if cmp.visible() then ]]
-		--[[ 		cmp.select_next_item() ]]
-		--[[ 	elseif luasnip.expand_or_jumpable() then ]]
-		--[[ 		luasnip.expand_or_jump() ]]
-		--[[ 	elseif has_words_before() then ]]
-		--[[ 		cmp.complete() ]]
-		--[[ 	else ]]
-		--[[ 		fallback() ]]
-		--[[ 	end ]]
-		--[[ end, { "i", "s" }), ]]
-		--[[ ["<S-Tab>"] = cmp.mapping(function(fallback) ]]
-		--[[ 	if cmp.visible() then ]]
-		--[[ 		cmp.select_prev_item() ]]
-		--[[ 	elseif luasnip.jumpable(-1) then ]]
-		--[[ 		luasnip.jump(-1) ]]
-		--[[ 	else ]]
-		--[[ 		fallback() ]]
-		--[[ 	end ]]
-		--[[ end, { "i", "s" }), ]]
-		["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-		["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			elseif has_words_before() then
+				cmp.complete()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		--[[ ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }), ]]
+		--[[ ["<S-Tab>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }), ]]
 		["<C-k>"] = cmp.mapping.select_prev_item(),
 		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
