391c0dc56259b0e2283fa77001160f364e15aecb
Xin Tan <mactanxin@gmail.com>
2022-11-06 19:40:30 +0800

    move some plugins to after/plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,21 +2,16 @@ local impatient_ok, impatient = pcall(require, "impatient")
 if impatient_ok then
 	impatient.enable_profile()
 end
-
 require("core.globals")
 require("core.ayu")
 require("core.options")
 require("core.keymaps")
 require("core.nvim-cmp")
-require("core.theme")
 require("core.plugins")
 require("core.treesitter")
 require("core.toggleterm")
-require("core.colorizer")
 require("core.comment")
 require("core.cinnamon")
-require("core.autopairs")
-require("core.showMessage")
 require("core.startup-screen")
 require("core.dashboard")
 require("core.neo-tree")
@@ -24,23 +19,15 @@ require("core.bufferline")
 require("core.lualine")
 require("core.diffview")
 require("core.lspconfig")
-require("core.symbols-outline")
 require("core.gitsigns")
 require("core.peepsight")
 require("core.blankline")
---[[ require("core.goto-preview") ]]
 require("core.custom-vim-functions")
 require("core.lightbulb")
-require("core.tabline")
 require("core.lsp-status")
 require("core.null-ls")
---[[ require("core.modes") ]]
-require("core.minimap")
 require("core.lspsaga")
-require("core.neodim")
-require("core.autotag")
 require("core.utils")
---[[ require("core.shade") ]]
 
 local vim = vim
 local execute = vim.api.nvim_command
