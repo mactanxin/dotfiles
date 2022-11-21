e8918cb4dbe3da65d66774d0817a9c91f2e1c261
Xin Tan <mactanxin@gmail.com>
2022-11-21 16:34:12 +0800

    mason config

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -136,7 +136,8 @@ return packer.startup(function(use)
 
 	-- LSP
 	use("neovim/nvim-lspconfig") -- enable LSP
-	use("williamboman/nvim-lsp-installer") -- simple to use language server installer
+	use({ "williamboman/mason.nvim" })
+  use({ "williamboman/mason-lspconfig.nvim" })
 	use("nvim-lua/lsp-status.nvim")
 	use("lukas-reineke/lsp-format.nvim")
 	use("onsails/lspkind.nvim")
