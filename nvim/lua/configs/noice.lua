076c98c594d87643bd70b9802ebd57f3ff2615b0
Xin Tan <mactanxin@gmail.com>
2022-12-01 12:21:19 +0800

    update icons

--- nvim/lua/configs/noice.lua
+++ nvim/lua/configs/noice.lua
@@ -15,6 +15,21 @@ require("noice").setup({
 			["cmp.entry.get_documentation"] = true,
 		},
 	},
+	format = {
+      -- conceal: (default=true) This will hide the text in the cmdline that matches the pattern.
+      -- view: (default is cmdline view)
+      -- opts: any options passed to the view
+      -- icon_hl_group: optional hl_group for the icon
+      -- title: set to anything or empty string to hide
+      cmdline = { pattern = "^:", icon = "", lang = "vim" },
+      search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
+      search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
+      filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
+      lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+      help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+      input = {}, -- Used by input()
+      -- lua = false, -- to disable a format, set to `false`
+    },
 	views = {
 		cmdline_popup = {
 			position = {
